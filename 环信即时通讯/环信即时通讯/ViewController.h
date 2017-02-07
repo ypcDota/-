03c44878bf93bbf710640d4ca4bd2a2275885609
PCweibo <985348196@qq.com>
2017-02-07 10:34:56 +0800

    创建项目

---
+++ 环信即时通讯/环信即时通讯/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  环信即时通讯
+//
+//  Created by ypc on 17/2/7.
+//  Copyright © 2017年 com.ypc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 环信即时通讯/环信即时通讯/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  环信即时通讯
+//
+//  Created by ypc on 17/2/7.
+//  Copyright © 2017年 com.ypc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
